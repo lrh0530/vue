c111474461cfe5886fb08b79aad707028ded9865
lrh0530 <17612760530@163.com>
2020-04-15 15:30:24 +0800

    vue-study-demo 工具栏demo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
+    <mynavbar></mynavbar>
     <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import mynavbar from './components/AppComponents/Navbar.vue'
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    mynavbar
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir',Helvetica,Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #42b983;
+  margin-top: 0px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import FirstPage from '@/components/FirstPage'
+import SecondPage from '@/components/SecondPage'
+import FirstPart from '@/components/HomeComponents/FirstPart'
+import SecondPart from '@/components/HomeComponents/SecondPart'
+import AppNavbar from '@/components/AppComponents/Navbar'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/Hello',
+      path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'FirstPart',
+          component: FirstPart
+        },
+        {
+          path: 'SecondPart',
+          name: 'SecondPart',
+          component: SecondPart
+        },
+        {
+          path: 'AppNavbar',
+          name: 'AppNavbar',
+          component: AppNavbar
+        }
+      ]
+    },
+    {
+      path: '/FirstPage',
+      name: 'FirstPage',
+      component: FirstPage
+    },
+    {
+      path: '/SecondPage',
+      name: 'SecondPage',
+      component: SecondPage
     }
   ]
 })
